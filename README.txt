e924490ec1f784ee286a3fc5db70c0b8bfa12398
JeanMichel FRANCOIS <toutpt@gmail.com>
2009-06-10 20:05:28 +0000

    initial import

---
+++ README.txt
@@ -0,0 +1,12 @@
+Introduction
+============
+
+Simple integration of jqueryui in Plone, like collective.js.jquery.
+
+It is different from collective.jqueryui in many ways:
+
+* no skin dirs (js and css are in resource dirs)
+* no all the bunch of files and documentations from original jqueryui
+* this one is minified
+* just add all jqueryui to portal_js, and default css to portal_css
+
---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.7.2.1'
+
+setup(name='collective.js.jqueryui',
+      version=version,
+      description="JQueryUI integration in portal_javascript",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='',
+      author='JeanMichel FRANCOIS aka toutpt',
+      author_email='jeanmichel.francois@makina-corpus.com',
+      url='https://svn.plone.org/svn/collective/collective.js.jqueryui',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['collective', 'collective.js'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'collective.js.jquery'
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
