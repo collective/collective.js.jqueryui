80dd42e85a1619b3ef6131e215854ddbc0138b5c
Vincent Fretin <vincent.fretin@gmail.com>
2010-03-17 10:00:10 +0000

    Select now the good jqueryui language and set the date format string from the date_format_short_datepicker msgid

--- collective/js/jqueryui/viewlet.py
+++ collective/js/jqueryui/viewlet.py
@@ -1,33 +1,52 @@
 from plone.app.layout.viewlets.common import ViewletBase
 from zope.i18n import translate
 
+# this list was generated from jqueryui 1.8rc3
+JQUERYUI_LANGUAGES = (
+    'af', 'ar', 'az', 'bg', 'bs', 'ca', 'cs', 'da', 'de', 'el', 'en-GB', 'eo',
+    'es', 'et', 'eu', 'fa', 'fi', 'fr-CH', 'fr', 'he', 'hr', 'hu', 'hy', 'id',
+    'is', 'it', 'ja', 'ko', 'lt', 'lv', 'ms', 'nl', 'no', 'pl', 'pt-BR', 'ro',
+    'ru', 'sk', 'sl', 'sq', 'sr', 'sr-SR', 'sv', 'th', 'tr', 'uk', 'vi',
+    'zh-CN', 'zh-HK', 'zh-TW',
+)
 
 class L10nDatepicker(ViewletBase):
 
-    def language(self):
+    def jq_language(self):
         language = self.request.LANGUAGE
         if '-' in language:
-            # strip combined language code
-            language = language.split('-')[0]
-        return language
+            # normalize combined language code
+            parts = language.split('-')
+            language = "%s-%s" % (parts[0], parts[1].upper())
+            if language in JQUERYUI_LANGUAGES:
+                return language
+            else:
+                # If the combined language doesn't exist in jqueryui
+                # check only the first part.
+                language = language.split('-')[0]
+        if language in JQUERYUI_LANGUAGES:
+            return language
+        # Return empty string if the language is not supported
+        # by jqueryui so the default this.regional[''] of the datepicker
+        # plugin is used.
+        return ''
 
     def update(self):
-        date_format = translate("date_format_short",
+        date_format = translate(u"date_format_short_datepicker",
                                 domain="plonelocales",
                                 context=self.request)
-        self.jq_date_format = date_format.replace(
-            "${d}", "dd").replace(
-            "${m}", "mm").replace(
-            "${Y}", "yy")
+        if date_format == u"date_format_short_datepicker":
+            self.jq_date_format = 'mm/dd/yy'
+        else:
+            self.jq_date_format = date_format
 
     def render(self):
-        print "rendering viewlet"
         return u"""<script type="text/javascript">
         jQuery(function($){
             $.datepicker.setDefaults(
                 jQuery.extend($.datepicker.regional['%s'],
                 {dateFormat: '%s'}));
         });
-        </script>""" % (self.language(), self.jq_date_format)
+        </script>""" % (self.jq_language(), self.jq_date_format)
     
 
