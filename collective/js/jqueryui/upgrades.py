5d631555c2481eeff9763d17e8c4c6bd7a1aae51
JeanMichel FRANCOIS <toutpt@gmail.com>
2011-01-07 16:05:42 +0000

    Update profile to use jquery-ui.min.js in portal_javascript and add migrations

--- collective/js/jqueryui/upgrades.py
+++ collective/js/jqueryui/upgrades.py
@@ -1,8 +1,17 @@
 from Products.CMFCore.utils import getToolByName
 
-
 def install_browserlayer(context):
     setup = getToolByName(context, 'portal_setup')
     setup.runImportStepFromProfile('profile-collective.js.jqueryui:default',
                                    'browserlayer', run_dependencies=False,
                                    purge_old=False)
+
+PREVIOUS = ('++resource++jquery-ui-1.8.min.js',
+            '++resource++jquery-ui-1.7.min.js',
+            '++resource++jquery-ui-1.7.2.min.js')
+
+def portal_javascripts(context):
+    
+    jsregistry = getToolByName(context, 'portal_javascripts')
+    for PREV in PREVIOUS:
+        jsregistry.unregisterResource(PREV)
