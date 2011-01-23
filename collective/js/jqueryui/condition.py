14179eb7f04797dbccbfed17151b41250f4261fa
JeanMichel FRANCOIS <toutpt@gmail.com>
2011-01-23 14:17:24 +0000

    replace restrictedTraverse with component.getMultiAdapter. remove @@ from id check

--- collective/js/jqueryui/condition.py
+++ collective/js/jqueryui/condition.py
@@ -1,3 +1,4 @@
+from zope import component
 from plone.memoize.view import memoize
 import logging
 logger = logging.getLogger('collective.js.jqueryui')
@@ -13,13 +14,14 @@ class IncludeJQueryUI(BrowserView):
             return True
 
         include_ids = pp.views_and_templates
-        context_state = self.context.restrictedTraverse('plone_context_state')
+        context_state = component.getMultiAdapter((self.context, self.request),
+                                                  name='plone_context_state')
         if context_state.is_view_template():
             template_id = context_state.view_template_id()
             logger.info(template_id)
             return template_id in include_ids
 
         current_url = context_state.current_page_url()
-        view_id = current_url.split('/')[-1]
+        view_id = current_url.split('/')[-1].replace('@@','')
         logger.info(view_id)
         return view_id in include_ids
