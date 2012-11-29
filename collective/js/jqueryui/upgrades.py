8a3b63da98eae5ea59a0bf6a8329f1f8edeaa737
JeanMichel FRANCOIS <toutpt@gmail.com>
2012-11-29 21:35:55 +0100

    rename effects to ui_effcts in registry.xml. remove old effects records

--- collective/js/jqueryui/upgrades.py
+++ collective/js/jqueryui/upgrades.py
@@ -1,7 +1,18 @@
+from zope import component
 from Products.CMFCore.utils import getToolByName
+from plone.registry.interfaces import IRegistry
 PROFILE = 'profile-collective.js.jqueryui:default'
 
 
 def common(context):
     setup = getToolByName(context, 'portal_setup')
     setup.runAllImportStepsFromProfile(PROFILE)
+
+    #cleanup from 1.8
+    registry = component.queryUtility(IRegistry)
+    for i in ('core','blind', 'bounce', 'clip', 'drop', 'explode',
+              'fade', 'fold', 'highlight', 'pulsate', 'scale',
+              'shake', 'slide', 'transfer'):
+        record_id = 'collective.js.jqueryui.controlpanel.IJQueryUIPlugins.effects_%s'%i
+        if record_id in registry.records:
+            del registry.records[record_id]
