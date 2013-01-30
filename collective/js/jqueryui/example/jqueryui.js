08dac00d52c0c9f41a6100946c6c7fe3c7a21e52
JeanMichel FRANCOIS <toutpt@gmail.com>
2013-01-30 15:57:17 +0100

    update the example page

--- collective/js/jqueryui/example/jqueryui.js
+++ collective/js/jqueryui/example/jqueryui.js
@@ -1,49 +1,96 @@
-jQuery(function(){
-    // Accordion
-    $("#accordion").accordion({ header: "h3" });
-    // Autocomplete
-    $("#autocomplete").autocomplete({
-        source: ["c++", "java", "php", "coldfusion", "javascript", "asp", "ruby", "python", "c", "scala", "groovy", "haskell", "perl"]
-    });
-    // Button
-    $("#button").button();
-    $("#radioset").buttonset();
-    // Tabs
-    $('#tabs').tabs();
-    // Dialog
-    $('#dialog').dialog({
-        autoOpen: false,
-        width: 600,
-        buttons: {
-            "Ok": function() {
-                $(this).dialog("close");
-            },
-            "Cancel": function() {
-                $(this).dialog("close");
-            }
-        }
-    });
-    // Dialog Link
-    $('#dialog_link').click(function(){
-        $('#dialog').dialog('open');
-        return false;
-    });
-    // Datepicker
-    $('#datepicker').datepicker({
-        inline: true
-    });
-    // Slider
-    $('#slider').slider({
-        range: true,
-        values: [17, 67]
-    });
-    // Progressbar
-    $("#progressbar").progressbar({
-        value: 20
-    });
-    //hover states on the static widgets
-    $('#dialog_link, ul#icons li').hover(
-        function() { $(this).addClass('ui-state-hover'); },
-        function() { $(this).removeClass('ui-state-hover'); }
-    );
-});
+	$(function() {
+		
+		$( "#accordion" ).accordion();
+		
+
+		
+		var availableTags = [
+			"ActionScript",
+			"AppleScript",
+			"Asp",
+			"BASIC",
+			"C",
+			"C++",
+			"Clojure",
+			"COBOL",
+			"ColdFusion",
+			"Erlang",
+			"Fortran",
+			"Groovy",
+			"Haskell",
+			"Java",
+			"JavaScript",
+			"Lisp",
+			"Perl",
+			"PHP",
+			"Python",
+			"Ruby",
+			"Scala",
+			"Scheme"
+		];
+		$( "#autocomplete" ).autocomplete({
+			source: availableTags
+		});
+		
+
+		
+		$( "#button" ).button();
+		$( "#radioset" ).buttonset();
+		
+
+		
+		$( "#dialog" ).dialog({
+			autoOpen: false,
+			width: 400,
+			buttons: [
+				{
+					text: "Ok",
+					click: function() {
+						$( this ).dialog( "close" );
+					}
+				},
+				{
+					text: "Cancel",
+					click: function() {
+						$( this ).dialog( "close" );
+					}
+				}
+			]
+		});
+
+		// Link to open the dialog
+		$( "#dialog-link" ).click(function( event ) {
+			$( "#dialog" ).dialog( "open" );
+			event.preventDefault();
+		});
+		
+
+		
+		$( "#datepicker" ).datepicker({
+			inline: true
+		});
+		
+
+		
+		$( "#slider" ).slider({
+			range: true,
+			values: [ 17, 67 ]
+		});
+		
+
+		
+		$( "#progressbar" ).progressbar({
+			value: 20
+		});
+		
+
+		// Hover states on the static widgets
+		$( "#dialog-link, #icons li" ).hover(
+			function() {
+				$( this ).addClass( "ui-state-hover" );
+			},
+			function() {
+				$( this ).removeClass( "ui-state-hover" );
+			}
+		);
+	});
