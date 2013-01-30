1956851c09e49309182a5c765ced697657133a51
JeanMichel FRANCOIS <toutpt@gmail.com>
2013-01-30 15:57:08 +0100

    add upgrade step

--- collective/js/jqueryui/upgrades.py
+++ collective/js/jqueryui/upgrades.py
@@ -10,12 +10,19 @@ def common(context):
 
     #cleanup from 1.8
     registry = component.queryUtility(IRegistry)
-    for i in ('core','blind', 'bounce', 'clip', 'drop', 'explode',
+    effect = 'collective.js.jqueryui.controlpanel.IJQueryUIPlugins.effects_%s'
+    ui = 'collective.js.jqueryui.controlpanel.IJQueryUIPlugins.ui_effects_%s'
+    for i in ('core', 'blind', 'bounce', 'clip', 'drop', 'explode',
               'fade', 'fold', 'highlight', 'pulsate', 'scale',
               'shake', 'slide', 'transfer'):
-        record_id = 'collective.js.jqueryui.controlpanel.IJQueryUIPlugins.effects_%s'%i
+        record_id = effect % i
         if record_id in registry.records:
             del registry.records[record_id]
-        record_id = 'collective.js.jqueryui.controlpanel.IJQueryUIPlugins.ui_effects_%s'%i
+        record_id = ui % i
         if record_id in registry.records:
             del registry.records[record_id]
+
+    css = getToolByName(context, 'portal_css')
+    css.cookResources()
+    js = getToolByName(context, 'portal_javascripts')
+    js.cookResources()
