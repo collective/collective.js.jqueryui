b234fe22be1387c322550e0fab9d73658dcf2d99
Robert Niederreiter <office@squarewave.at>
2016-04-04 19:53:19 +0200

    Cleanup GS profiles to install properly in Plone 5. Simplify resource registration for Plone 5 the way done in https://github.com/collective/example.p4p5#cssjs-declaration-in-plone-5

--- collective/js/jqueryui/setuphandlers.py
+++ collective/js/jqueryui/setuphandlers.py
@@ -1,15 +1,33 @@
-from zope.component._api import getUtility
-from plone.registry.interfaces import IRegistry
-
-from collective.js.jqueryui.interfaces import IJQueryUIPlugins
+from Products.CMFPlone import interfaces as Plone
+from Products.CMFQuickInstallerTool import interfaces as QuickInstaller
 from collective.js.jqueryui.controlpanel import verify_jsregistry
+from collective.js.jqueryui.interfaces import IJQueryUIPlugins
+from plone.registry.interfaces import IRegistry
+from zope.component._api import getUtility
+from zope.interface import implementer
 
 
 def applySettings(gscontext):
     # don't run as a step for other profiles
     if gscontext.readDataFile('jqueryui.txt') is None:
         return
-
     record = getUtility(IRegistry).forInterface(IJQueryUIPlugins)
-
     verify_jsregistry(record)
+
+
+@implementer(Plone.INonInstallable)
+class HiddenProfiles(object):
+
+    def getNonInstallableProfiles(self):
+        """Do not show on Plone's list of installable profiles.
+        """
+        return ['collective.js.jqueryui:install-base']
+
+
+@implementer(QuickInstaller.INonInstallable)
+class HiddenProducts(object):
+
+    def getNonInstallableProducts(self):
+        """Do not show on QuickInstaller's list of installable products.
+        """
+        return ['collective.js.jqueryui:install-base']
