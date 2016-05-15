4ee9487a4a4db14430dfc603b70ddbf5258c51fa
Maurits van Rees <maurits@vanrees.org>
2016-05-15 19:52:32 +0200

    Reindented setup.py, renamed docs/HISTORY.txt to CHANGES.rst.
    [ci skip]

--- setup.py
+++ setup.py
@@ -3,44 +3,43 @@ import os
 
 version = '2.0.2.dev0'
 
-setup(name='collective.js.jqueryui',
-      version=version,
-      description="JQueryUI ready for Plone",
-      long_description=open("README.rst").read() + "\n" +
-           open(os.path.join("docs", "UPGRADE.txt")).read() + "\n" +
-           open(os.path.join("docs", "HISTORY.txt")).read(),
-      classifiers=[
+setup(
+    name='collective.js.jqueryui',
+    version=version,
+    description="JQueryUI ready for Plone",
+    long_description=open("README.rst").read() + "\n" +
+         open(os.path.join("docs", "UPGRADE.txt")).read() + "\n" +
+         open("CHANGES.rst").read(),
+    classifiers=[
         "Environment :: Web Environment",
         "License :: OSI Approved :: GNU General Public License (GPL)",
         "Operating System :: OS Independent",
         "Framework :: Zope2",
         "Framework :: Plone",
-        "Framework :: Plone :: 4.1",
-        "Framework :: Plone :: 4.2",
         "Framework :: Plone :: 4.3",
+        "Framework :: Plone :: 5.0",
         "Programming Language :: Python",
         "Programming Language :: Python :: 2.6",
         "Programming Language :: Python :: 2.7",
-      ],
-      keywords='plone jqueryui',
-      author='JeanMichel FRANCOIS aka toutpt',
-      author_email='toutpt@gmail.com',
-      url='https://github.com/collective/collective.js.jqueryui',
-      license='GPL',
-      packages=find_packages(exclude=['ez_setup']),
-      namespace_packages=['collective', 'collective.js'],
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=[
-          'setuptools',
-          'plone.app.jquery >1.6'
-          # -*- Extra requirements: -*-
-      ],
-      setup_requires=["setuptools_git"],
-      extras_require={},
-      entry_points="""
-      # -*- Entry points: -*-
-      [z3c.autoinclude.plugin]
-      target = plone
-      """,
-      )
+    ],
+    keywords='plone jqueryui',
+    author='JeanMichel FRANCOIS aka toutpt',
+    author_email='toutpt@gmail.com',
+    url='https://github.com/collective/collective.js.jqueryui',
+    license='GPL',
+    packages=find_packages(exclude=['ez_setup']),
+    namespace_packages=['collective', 'collective.js'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'setuptools',
+        'plone.app.jquery > 1.6'
+    ],
+    setup_requires=["setuptools_git"],
+    extras_require={},
+    entry_points="""
+    # -*- Entry points: -*-
+    [z3c.autoinclude.plugin]
+    target = plone
+    """,
+    )
