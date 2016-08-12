0e9e0ed0c5ea39ce6929927de550a0a304bd6e7c
Maurits van Rees <maurits@vanrees.org>
2016-08-12 22:36:01 +0200

    Added Plone 5.1 classifier to setup.py.
        [ci skip]

--- setup.py
+++ setup.py
@@ -18,6 +18,7 @@ setup(
         "Framework :: Plone",
         "Framework :: Plone :: 4.3",
         "Framework :: Plone :: 5.0",
+        "Framework :: Plone :: 5.1",
         "Programming Language :: Python",
         "Programming Language :: Python :: 2.6",
         "Programming Language :: Python :: 2.7",
