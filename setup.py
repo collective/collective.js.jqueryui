5c6d39ed189cbb5fc9b0cb9190da2564f831cec0
wesleybl <wesleybl@gmail.com>
2022-04-21 09:34:53 -0300

    Fix python_requires

--- setup.py
+++ setup.py
@@ -55,7 +55,7 @@ setup(
     package_dir={"": "src"},
     include_package_data=True,
     zip_safe=False,
-    python_requires=">=3.7",
+    python_requires=">=2.7",
     install_requires=[
         "plone.app.jquery > 1.6",
         "Products.CMFPlone",
