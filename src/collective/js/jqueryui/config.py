71f154762563dada6248a0f3b3d17e9d2002fe1b
Tibor Szakmany <tibor.szakmany@eaudeweb.ro>
2023-06-20 10:59:21 +0200

    fix resource paths

--- src/collective/js/jqueryui/config.py
+++ src/collective/js/jqueryui/config.py
@@ -2,5 +2,12 @@ VERSION = "1.13.2"
 
 # minimized JS files in the `jquery-ui-<VERSION>.custom` folder
 JQUERYUI_RESOURCES = [
-    "++resource++jquery-ui/jqueryui.min.js",
+    "++resource++jquery-ui/jquery-ui.min.js",
+]
+
+# minimized CSS files in the `jquery-ui-<VERSION>.custom` folder
+CSS_RESOURCES = [
+    "++resource++jquery-ui/jquery-ui.min.css",
+    "++resource++jquery-ui/jquery-ui.structure.min.css",
+    "++resource++jquery-ui/jquery-ui.theme.min.css",
 ]
