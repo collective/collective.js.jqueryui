98cd6f28dc7b34f888ffc6b6f54e1535fddefd8e
Tibor Szakmany <tibor.szakmany@eaudeweb.ro>
2023-06-21 08:14:11 +0200

    fix require.js error
    Fixes the error raised when it tries to register as AMD anonymous module.

--- src/collective/js/jqueryui/jquery-ui-1.13.2.custom/jquery-ui.js
+++ src/collective/js/jqueryui/jquery-ui-1.13.2.custom/jquery-ui.js
@@ -5,16 +5,10 @@
 
 ( function( factory ) {
 	"use strict";
-	
-	if ( typeof define === "function" && define.amd ) {
-
-		// AMD. Register as an anonymous module.
-		define( [ "jquery" ], factory );
-	} else {
 
 		// Browser globals
 		factory( jQuery );
-	}
+
 } )( function( $ ) {
 "use strict";
 
